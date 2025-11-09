a17fc7a9050699389d37c5ca5cf11e876ac41948
basurakid <92126346+basurakid@users.noreply.github.com>
2025-11-09 18:10:05 +0100

    add usermedia and tesseract functions, basic html

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import "./style.css";
+import { createWorker } from 'tesseract.js';
 
+const cameraBtn = document.querySelector(".cameraBtn")
+
+async function getMedia(constraints) {
+  let stream = null;
+try {
+    stream = await navigator.mediaDevices.getUserMedia(constraints);
+    /* use the stream */
+  } catch (err) {
+    /* handle the error */
+  }
+}
+
+(async () => {
+
+  const worker = await createWorker('eng');
+  const ret = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
+  console.log(ret.data.text);
+  await worker.terminate();
+})();
 
 
 
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,12 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('path');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
+    hot:true,
   },
 });
